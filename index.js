87f5cc1ae7cc17b78947f441c475739df6d1aca5
Ngoc Linh Pham <pnlinh1207@gmail.com>
2019-03-14 11:50:46 +0700

    Init

---
+++ index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const app = express();
+const PORT = 3000;
+
+app.listen(PORT, () => {
+    console.log(`Server listening in port: ${PORT}`);
+});
+
+app.get('/', (req, res) => {
+    res.setHeader('Content-type', 'text/html; charset=utf-8');
+    res.send('<h1 style="color: red">Hello Nodejs</h1>');
+});
+
+app.get('/about', (req, res) => {
+    res.send('<h1>This is about page</h1>');
+});
